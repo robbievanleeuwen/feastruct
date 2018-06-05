9ab02fa71fffd404109464746f8cadca5279b20b
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2018-06-05 14:53:56 +0200

    Add mock modules to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,12 @@
 #
 import os
 import sys
+import mock
+
+MOCK_MODULES = ['numpy', 'matplotlib.patches']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 sys.path.insert(0, os.path.abspath('../../'))
 
 
